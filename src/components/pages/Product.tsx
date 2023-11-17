d6495c9af510a7a4349fd28ca9bf3809acc99512
Reydel Ocon <reydel321@gmail.com>
2023-11-18 01:42:18 +0800

    added many changes

--- src/components/pages/Product.tsx
+++ src/components/pages/Product.tsx
@@ -2,9 +2,32 @@ import PageHeader from '../PageHeader';
 import dumy from '@/assets/dumy.png';
 import Rainbow from '@/assets/RAINBOW.jpg';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import axios from 'axios';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+type SupplierDetails = {
+  supplier_name: string;
+  phone: string;
+  product_supplied: string;
+  address: string;
+  supplier_id: number;
+};
+
 export default function Product() {
   const [isMouseOver, setIsMouseOver] = useState<boolean>(false);
+  const [showProductModal, setShowProductModal] = useState<boolean>(false);
+  const [productDetails, setProductDetails] = useState({});
+  const [suppliers, setSuppliers] = useState<SupplierDetails[]>([]);
+  const [supplierID, setSupplierID] = useState<number>(0);
 
   const handleMouseOver = () => {
     setIsMouseOver(true);
@@ -14,8 +37,37 @@ export default function Product() {
     setIsMouseOver(false);
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setProductDetails((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('dsadas');
+    e.preventDefault();
+    axios
+      .post('http://localhost/jed-inventory/supplier.php', {
+        ...productDetails,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  const getAllSuppliers = () => {
+    axios.get('http://localhost/jed-inventory/supplier.php').then((res) => {
+      console.log(res.data);
+      setSuppliers(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllSuppliers();
+  }, []);
+
   return (
-    <div className="p-4">
+    <div className="p-4 relative">
       <div className="flex gap-10">
         <div className="w-[80%]">
           <PageHeader
@@ -48,6 +100,15 @@ export default function Product() {
         </div>
 
         <div className="w-full">
+          <div className="flex gap-2 mb-2 w-full items-end justify-end">
+            <Button
+              onClick={() => setShowProductModal(true)}
+              className="bg-pink-500"
+            >
+              Add Product
+            </Button>
+            <Button className="bg-pink-500">Add Product</Button>
+          </div>
           <div className="grid grid-cols-2 gap-2 w-full">
             <div
               onMouseOver={() => handleMouseOver()}
@@ -107,6 +168,77 @@ export default function Product() {
           </div>
         </div>
       </div>
+
+      {showProductModal && (
+        <div className="absolute w-full h-full top-0 z-40 bg-white bg-opacity-80 flex justify-center items-center">
+          <form
+            className="bg-white w-[35rem] p-4 rounded-md border-pink-500 border-2"
+            onSubmit={handleSubmit}
+          >
+            <Label>Supplier</Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Supplier" />
+              </SelectTrigger>
+              <SelectContent>
+                {suppliers.map((supplier) => {
+                  return (
+                    <SelectItem
+                      onClick={() => setSupplierID(supplier.supplier_id)}
+                      value={supplier.supplier_name}
+                    >
+                      {supplier.supplier_name}
+                    </SelectItem>
+                  );
+                })}
+              </SelectContent>
+            </Select>
+            <div>
+              <Label>Product Image</Label>
+              <Input
+                type="file"
+                onChange={handleInputChange}
+                name="product_image"
+              />
+            </div>
+            <div>
+              <Label>Product Name</Label>
+              <Input onChange={handleInputChange} name="product_name" />
+            </div>
+
+            <div>
+              <Label>Description</Label>
+              <Input onChange={handleInputChange} name="description" />
+            </div>
+
+            <div>
+              <Label>Stocks</Label>
+              <Input onChange={handleInputChange} name="stocks" />
+            </div>
+
+            <div>
+              <Label>Expiration</Label>
+              <Input
+                type="date"
+                onChange={handleInputChange}
+                name="expiration_date"
+              />
+            </div>
+
+            <div className="gap-2 flex">
+              <Button
+                onClick={() => setShowProductModal(false)}
+                className="mt-2"
+              >
+                Cancel
+              </Button>
+              <Button type="submit" className="mt-2">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/pages/Supplier.tsx
+++ src/components/pages/Supplier.tsx
@@ -1,6 +1,59 @@
 import PageHeader from '../PageHeader';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
+import { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
+type SupplierDetails = {
+  supplier_name: string;
+  phone: string;
+  product_supplied: string;
+  address: string;
+};
 export default function Supplier() {
+  const [supplierDetails, setSupplierDetails] = useState({});
+  const [suppliers, setSuppliers] = useState<SupplierDetails[]>([]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setSupplierDetails((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('dsadas');
+    e.preventDefault();
+    axios
+      .post('http://localhost/jed-inventory/supplier.php', {
+        ...supplierDetails,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  const getAllSuppliers = () => {
+    axios.get('http://localhost/jed-inventory/supplier.php').then((res) => {
+      console.log(res.data);
+      setSuppliers(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllSuppliers();
+  }, []);
+
   return (
     <div className="border-2 p-4">
       <div>
@@ -10,7 +63,66 @@ export default function Supplier() {
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum et cum eveniet ut saepe commodi, incidunt voluptas nostrum? Quasi, cupiditate!"
         />
       </div>
-      <div>hdiasbd</div>
+      <div className="mt-[2rem]">
+        <div className="flex w-full justify-between gap-10">
+          <div className="w-[40rem] border-2 p-2">
+            <h1 className="font-bold text-2xl">Supplier Info</h1>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <Label>Supplier Name</Label>
+                <Input onChange={handleInputChange} name="supplier_name" />
+              </div>
+
+              <div>
+                <Label>Phone</Label>
+                <Input onChange={handleInputChange} name="phone" />
+              </div>
+
+              <div>
+                <Label>Product Supplied</Label>
+                <Input onChange={handleInputChange} name="product_supplied" />
+              </div>
+
+              <div>
+                <Label>Address</Label>
+                <Input onChange={handleInputChange} name="address" />
+              </div>
+
+              <Button type="submit" className="mt-2">
+                Submit
+              </Button>
+            </form>
+          </div>
+
+          <div className="w-full">
+            <Table className="border-2">
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Supplier</TableHead>
+                  <TableHead>Product Supplied</TableHead>
+                  <TableHead>Address</TableHead>
+                  <TableHead>Phone</TableHead>
+                  <TableHead>Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {suppliers.map((supplier) => {
+                  return (
+                    <TableRow>
+                      <TableCell>{supplier.supplier_name}</TableCell>
+                      <TableCell>{supplier.product_supplied}</TableCell>
+                      <TableCell>{supplier.address}</TableCell>
+                      <TableCell>{supplier.phone}</TableCell>
+                      <TableCell>delete update</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
